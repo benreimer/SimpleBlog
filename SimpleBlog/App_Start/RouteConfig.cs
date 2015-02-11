d368475c928667fe24a1cc22ab47be6ab4643db6
unknown <BR022737@CERN-BR022737.cerner.com>
2015-02-11 00:14:49 -0500

    Lecture 7:  Added basic routes and controllers and the admin area.

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
+
 
 namespace SimpleBlog
 {
@@ -11,13 +13,14 @@ namespace SimpleBlog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            var namespaces = new[] { typeof(PostsController).Namespace };
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            routes.MapRoute("Login", "login", new { Controller ="Auth", Action="Login"},namespaces);
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Home", "", new { Controller = "Posts", Action = "Index" },namespaces);
+
         }
     }
 }
