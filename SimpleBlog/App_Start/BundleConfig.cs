b0d732f2105a2c4d64162f752743d8b42f8b0648
unknown <BR022737@CERN-BR022737.northamerica.cerner.net>
2015-02-13 12:32:22 -0500

    Lecture 11: Imported styles and scripts (jQuery and Bootstrap)

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundles.Add(new StyleBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+            
+            bundles.Add(new StyleBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+        }
+    }
+}
--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Optimization;
+
 
 namespace SimpleBlog
 {
@@ -19,6 +21,8 @@ namespace SimpleBlog
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
         }
     }
 }
