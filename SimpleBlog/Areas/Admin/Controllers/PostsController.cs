00088525f4b7d25e5c67b0a28251d6e1046d945d
Ben Reimer <reimer.ben@gmail.com>
2015-02-25 00:01:10 -0500

    Lecture 24: Created new and edit actions for the posts controller

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using SimpleBlog.Infrastructure;
 using NHibernate.Linq;
 using SimpleBlog.Areas.Admin.ViewModels;
+using System;
 
 
 namespace SimpleBlog.Areas.Admin.Controllers
@@ -30,6 +31,63 @@ namespace SimpleBlog.Areas.Admin.Controllers
                {
                    Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
                });
+        }
+
+        public  ActionResult New()
+        {
+            return View("Form", new PostsForm
+                {
+                    isNew = true
+                });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm
+            {
+                isNew = false,
+                PostId = id,
+            Content = post.Content,
+            Slug = post.Slug,
+            Title = post.Title
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Form(PostsForm form)
+        {
+            form.isNew = form.PostId == null;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+                if(form.isNew)
+                {
+                    post = new Post
+                    {
+                        CreatedAt = DateTime.UtcNow, 
+                        User = Auth.User,
+                    };
+                }
+                else
+                {
+                    post = Database.Session.Load<Post>(form.PostId);
+                    if (post == null)
+                        return HttpNotFound();
+                    post.UpdatedAt = DateTime.UtcNow;
+                }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+            return RedirectToAction("Index");
         }
     }
 }
--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -1,10 +1,25 @@
 ﻿using SimpleBlog.Infrastructure;
 using SimpleBlog.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleBlog.Areas.Admin.ViewModels
 {
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; }
+    }
+
+    public class PostsForm
+    {
+        public bool isNew { get; set; }
+            public int? PostId{ get; set; }
+
+            [Required, MaxLength(128)]
+                public string Title{ get; set; }
+
+                    [Required, MaxLength(128)]
+                      public string Slug{ get; set; }
+                          [Required, DataType(DataType.MultilineText)]
+                    public string Content { get; set; }
     }
 }
